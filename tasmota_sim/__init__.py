f5222fea3be0f2a2a3c4160bc94e479f9e8b5eb7
ScharxiDev <scharxidev@outlook.de>
2025-06-13 12:51:38 +0200

    Add Tasmota Simulator package with initial version and author information

---
+++ tasmota_sim/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tasmota Simulator Package
+
+A Python application for simulating Tasmota smart plugs using RabbitMQ for communication.
+Each simulated device gets its own IP address within a Docker network.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Tasmota Simulator" 
